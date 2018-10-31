6f1a62f176e4d0122405ee4c94e19669348498dd
leo <linwei5316@gmail.com>
2018-10-31 23:27:01 +0800

    initial commit
    this version include vue-axios bootstrap api-path_env-variable settings

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created() {
+    const API = `${process.env.API_PATH}/api/${process.env.CUSTOM_API_PATH}/products`
+
+    this.$http.get(API)
+      .then(response => console.log(response))
+  },
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/all.scss';
+</style>
+
