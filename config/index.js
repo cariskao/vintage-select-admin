0a27a81367c98cf163eaba78bd64d7d31f62e268
leo <linwei5316@gmail.com>
2018-11-21 17:16:24 +0800

    再改正部署路徑

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/vintage-select-admin/',
     proxyTable: {},
 
     // Various Dev Server settings
